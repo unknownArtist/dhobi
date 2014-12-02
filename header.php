b4364c8f9e2f8c0d5c5bfcf722bfdeb0295fe3b1
Muhammad Saqib <rooott@gmail.com>
2014-12-02 11:46:00 +0500

    logout fixed

--- header.php
+++ header.php
@@ -99,7 +99,7 @@ else
                                         <a href="#" class="btn btn-default btn-flat">Profile</a>
                                     </div>
                                     <div class="pull-right">
-                                        <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                        <a href="logout.php" class="btn btn-default btn-flat">Sign out</a>
                                     </div>
                                 </li>
                             </ul>
